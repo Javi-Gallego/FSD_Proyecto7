243c2dc8116068980b970fd16290877416b743e9
Javi-Gallego <galgar@gmail.com>
2024-04-03 19:47:30 +0200

    fix: remove AuthButton and AuthInput and created MyButton and MyInput to use in all the project

--- src/common/AuthButton/AuthButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./AuthButton.css";
-
-export const AuthButton = ({ text, functionClick, currentClass }) => {
-  return (
-    <div onClick={functionClick} className={currentClass}>
-      {text}
-    </div>
-  );
-};
